210f264a689acb6fd416484c9c3c4c573c5c3d92
cephaspad <cephaspad@users.noreply.github.com>
2026-02-02 08:27:52 +0000

    Refactor: Update build script to consistently use the build directory for all build steps and copy final artifacts to the output directory.

--- specs/truongthanhoc-api-builder/dev/run.sh
+++ specs/truongthanhoc-api-builder/dev/run.sh
@@ -4,21 +4,16 @@ SOURCE=$1
 OUTPUT=$2
 BUILD=${3:-false}
 
-export PATH=$PATH:$OUTPUT/node_modules/.bin
+export PATH=$PATH:$BUILD/node_modules/.bin
 
-cd $SOURCE
-cp $SOURCE/package.json $OUTPUT
-cp $SOURCE/package-lock.json $OUTPUT
-
-cd $OUTPUT
-npm install -g node-gyp
-npm config set fetch-retry-maxtimeout 600000 -g && npm install --only=production
-
-cp -r $SOURCE/* $BUILD
+cp $SOURCE/* $BUILD
 
 cd $BUILD
+
+npm install -g node-gyp
+npm config set fetch-retry-maxtimeout 600000 -g && npm install --only=production
 npm run build
 
-cp -r $BUILD/dist $SOURCE/dist
+cp -r $BUILD/dist/* $OUTPUT
 
 exit 0
