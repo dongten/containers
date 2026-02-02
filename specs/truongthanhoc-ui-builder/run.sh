078a0b9584781ad9260f49c37c136bc3851f538a
cephaspad <cephaspad@users.noreply.github.com>
2026-02-02 13:11:37 +0000

    refactor: separate UI build logic into a dedicated builder image and script, simplifying the base runtime image.

---
+++ specs/truongthanhoc-ui-builder/run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+
+APP_SOURCE=${1:-/src}
+APP_OUTPUT=${2:-/dist}
+APP_BUILD=${3:-/app}
+
+export PATH=$PATH:$APP_BUILD/node_modules/.bin
+
+# mkdir if not exists
+mkdir -p $APP_BUILD
+mkdir -p $APP_OUTPUT
+
+cp -r $APP_SOURCE/* $APP_BUILD
+
+cd $APP_BUILD
+
+npm install -g node-gyp
+npm config set fetch-retry-maxtimeout 600000 -g && npm install --only=production
+npm run build
+
+cp -r /app/.next/standalone/* $APP_OUTPUT
+cp -r /app/.next/static $APP_OUTPUT/static
+
+exit 0
