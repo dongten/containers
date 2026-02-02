a08b2d90e37ddd1ae2122e77169f4cfe7d0d8cbc
cephaspad <cephaspad@users.noreply.github.com>
2026-02-02 09:18:35 +0000

    build: create build and output directories if missing

--- specs/truongthanhoc-api-builder/dev/run.sh
+++ specs/truongthanhoc-api-builder/dev/run.sh
@@ -7,6 +7,10 @@ APP_BUILD=${3:-/app}
 
 export PATH=$PATH:$APP_BUILD/node_modules/.bin
 
+# mkdir if not exists
+mkdir -p $APP_BUILD
+mkdir -p $APP_OUTPUT
+
 cp $APP_SOURCE/* $APP_BUILD
 
 cd $APP_BUILD
