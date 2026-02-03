5ed74393ee7253d94db593d29e5c176d2f778cbb
cephaspad <cephaspad@users.noreply.github.com>
2026-02-03 01:30:21 +0000

    chore: Streamline build process by removing intermediate build directory and associated file copy operations.

--- specs/truongthanhoc-ui-builder/dev/run.sh
+++ specs/truongthanhoc-ui-builder/dev/run.sh
@@ -3,21 +3,14 @@
 
 APP_SOURCE=${1:-/src}
 APP_OUTPUT=${2:-/dist}
-APP_BUILD=${3:-/app}
 
 export PATH=$PATH:$APP_BUILD/node_modules/.bin
 
 # mkdir if not exists
-mkdir -p $APP_BUILD
 mkdir -p $APP_OUTPUT
 
-cp -r $APP_SOURCE/* $APP_BUILD
-
-cd $APP_BUILD
-
-npm config set fetch-retry-maxtimeout 600000 -g && npm install
+npm config set fetch-retry-maxtimeout 600000 -g
+npm install
 next build
-
-cp -r $APP_BUILD/* $APP_OUTPUT
 
 exit 0
