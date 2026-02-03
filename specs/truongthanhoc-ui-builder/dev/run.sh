07bca9c325c9b797cb0afcf0be49e2519d297c8a
cephaspad <cephaspad@users.noreply.github.com>
2026-02-03 01:42:01 +0000

    chore (truongthanhoc-ui-builder): fix

--- specs/truongthanhoc-ui-builder/dev/run.sh
+++ specs/truongthanhoc-ui-builder/dev/run.sh
@@ -4,13 +4,13 @@
 APP_SOURCE=${1:-/src}
 APP_OUTPUT=${2:-/dist}
 
-export PATH=$PATH:$APP_BUILD/node_modules/.bin
-
 # mkdir if not exists
 mkdir -p $APP_OUTPUT
 
 npm config set fetch-retry-maxtimeout 600000 -g
 npm install
-npm run build
+
+export PATH=$PATH:$APP_OUTPUT/node_modules/.bin
+next build
 
 exit 0
